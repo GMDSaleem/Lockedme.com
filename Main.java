42200c74b1dfc5b04533e2cd8056226a2e92d4d8
GMDSaleem <gandikotashaiksaleem@gmail.com>
2022-02-24 00:05:47 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+	
+	public static final String path = "D:\\project tests"; 
+	
+	public static void main(String[] args) {
+		Screen menu = new Screen();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
